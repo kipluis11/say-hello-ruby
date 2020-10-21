90d1432efa7fb4d82cfa67834913946600a73c09
kip_parcell <kipluis@gmail.com>
2020-10-20 17:02:16 -0700

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
 # Build your say_hello method here
+
+# first neeed to build say_hello method
+# then define method with optional argument
+
+def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
+end
